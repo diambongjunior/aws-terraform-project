9ba96f729b82889cc61d321738ec1cf9711e964e
diambongjunior <103001403+diambongjunior@users.noreply.github.com>
2022-12-28 00:54:52 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = "us-east-1"
   version = "v2.70.0"
-  access_key = "AKIA5SWJ7PN7ET36NTND"
-  secret_key = "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"
+  access_key = "AKIA5SWJ7PN7H7RRHLOK"
+  secret_key = "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"
 }
